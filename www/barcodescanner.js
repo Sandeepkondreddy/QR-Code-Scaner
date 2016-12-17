87cdac8a4a09b036fa4ca52c83a648f2f685c0f8
Sandeep <san.alive@gmail.com>
2016-12-17 17:43:48 +0530

    //--------------------------------------------------------------------------

--- www/barcodescanner.js
+++ www/barcodescanner.js
@@ -122,6 +122,7 @@ BarcodeScanner.prototype.scan = function (successCallback, errorCallback, config
         };
 
         //--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
         BarcodeScanner.prototype.encode = function (type, data, successCallback, errorCallback, options) {
             if (errorCallback == null) {
                 errorCallback = function () {
